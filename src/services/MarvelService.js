fa703b1eaaada1dd1ffc9a64072c47ecfa3e62c8
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2024-12-16 16:35:37 +0300

    first commit, added RandomChar

---
+++ src/services/MarvelService.js
@@ -0,0 +1,38 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=290eb8100efa9dd2b4ec966db570116e';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+    
+        return await res.json();
+        
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+    
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+        return this._transformCharacter(res);
+    }  
+
+    _transformCharacter = (res) => {
+        return {
+            name: res.data.results[0].name,
+            description: res.data.results[0].description,
+            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+            homepage: res.data.results[0].urls[0].url,
+            wiki: res.data.results[0].urls[1].wiki
+        }
+    }
+}
+
+export default MarvelService;

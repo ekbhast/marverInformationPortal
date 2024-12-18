d69930412bbc846e97fb2a27705027a483bf7948
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2024-12-18 16:58:58 +0300

    added loader, and error

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import Spinner from '../spiner/Spinner';
+import ErrorMessage from '../errorMessage/errorMessage';
 import MarvelService from '../../services/MarvelService';
 
 class RandomChar extends Component {
@@ -13,52 +14,44 @@ class RandomChar extends Component {
 
     state = {
         char: {},
-        loading: true
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-        this.setState({char})
+        this.setState({
+            char,
+            loading: false})
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
-            .then(this.onCharLoaded);
+            .then(this.onCharLoaded)
+            .catch(this.onError);
     }
 
     render(){
-        let {char : {name, description, thumbnail, homepage, wiki}, loading} = this.state;
+        let {char, error,  loading} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spiner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
 
-        if (loading){
-            return <Spinner/>
-        }
-
-        if (!description){
-            description = 'Описание данного пресонажа будет добавлено позже';
-        } else if (description.length > 225) {
-            description = description.substr(0, 225) + `...`;
-            console.log(description.length);            
-        }
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">{description}</p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spiner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -76,6 +69,35 @@ class RandomChar extends Component {
         )
     }
     
+}
+
+const View = ({char}) => {
+    let {name, description, thumbnail, homepage, wiki} = char;
+
+    if (!description){
+        description = 'Описание данного пресонажа будет добавлено позже';
+    } else if (description.length > 225) {
+        description = description.substr(0, 225) + `...`;
+        console.log(description.length);            
+    }
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">{description}</p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default RandomChar;

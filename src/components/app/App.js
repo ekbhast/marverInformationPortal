7e7a3e850cd9343a5ce269ecfeee7eac903d08ec
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-01-24 16:38:12 +0300

    Added routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,40 +1,36 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
+import {MainPage, ComicsPage} from '../pages';
 
-import decoration from '../../resources/img/vision.png';
+
 
 const App = () => {
     
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
+    
 
     return (
+        <Router>
         <div className="app">
-            <AppHeader/>
+            <AppHeader />
             <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <ComicsList/>
+                <Routes>
+                    <Route 
+                        path="/" 
+                        element={
+                           <MainPage/>
+                        } 
+                    />
+                    <Route 
+                        path="/comics" 
+                        element={
+                            <ComicsPage/>
+                        } 
+                    />
+                </Routes>
             </main>
         </div>
+    </Router>
+
     )
 }
 
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,19 @@
 import './appHeader.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="http://localhost:3000/">
+                <Link to="/">
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="http://localhost:3000/">Characters</a></li>
+                    <li><NavLink style={({isActive}) => isActive ? {'color':'#9f0013'} : undefined} to="/">Characters</NavLink></li>
                     /
-                    <li><a href="http://localhost:3000/">Comics</a></li>
+                    <li><NavLink style={({isActive}) => isActive ? {'color':'#9f0013'} : undefined} to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
